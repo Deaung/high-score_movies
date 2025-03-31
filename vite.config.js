4f78ac99450903ecae0497097ad6122fa7ee6050
Deaung <a76650051@gmail.com>
2025-03-31 19:11:13 +0900

    feat:20250331 uploaded

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/high-score_movies/', //깃허브 리포지토리와 같게끔
   plugins: [react()],
 })
